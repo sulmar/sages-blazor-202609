89ee319beef8c582377b4d05f9249ec089cdcda1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-09-15 15:07:15 +0200

    Add dependency injection

--- src/BlazorApp/BlazorApp/Program.cs
+++ src/BlazorApp/BlazorApp/Program.cs
@@ -1,5 +1,6 @@
 using BlazorApp.Client.Pages;
 using BlazorApp.Components;
+using Infrastructure.Fakers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSingleton<CustomerFaker>();
 
 var app = builder.Build();
 
